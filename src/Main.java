a9ffe194fefe28bbb130f6703e8936f5f761bccc
thomasengels <thomas.engels92@gmail.com>
2016-01-10 16:27:19 +0100

    added ICON to application window

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GUI/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Chess Glenn & Thomas");
+        primaryStage.getIcons().add(new Image("GUI/Chess-Game.png"));
         SetupBoardRefactor setup = new SetupBoardRefactor();
         Game game = new Game();
         game.getAiPlayer().addObserver(setup);
